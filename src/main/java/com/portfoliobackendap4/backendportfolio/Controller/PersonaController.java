a873fd7cf056f56bd920e2894559caea1092a93d
german120 <germanblasco120@gmail.com>
2023-04-26 23:06:48 -0300

    Agregado todos los controladores
    Agregado todos los controladores, probado con postman de forma local, corregido el traer/perfiles que le faltaba definir cual ID traer

--- src/main/java/com/portfoliobackendap4/backendportfolio/Controller/PersonaController.java
+++ src/main/java/com/portfoliobackendap4/backendportfolio/Controller/PersonaController.java
@@ -4,6 +4,7 @@ import com.portfoliobackendap4.backendportfolio.Entity.Persona;
 import com.portfoliobackendap4.backendportfolio.Interface.IPersonaService;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,7 +15,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-
+@CrossOrigin(origins = "http://localhost:4200")
 public class PersonaController {
     @Autowired IPersonaService ipersonaService;
     
@@ -54,7 +55,7 @@ public class PersonaController {
     
         @GetMapping("/personas/traer/perfil")
         public Persona findPersona(){
-           return ipersonaService.findPersona(Long.MIN_VALUE);
+           return ipersonaService.findPersona((long)1);
         }
 
     
