7b559bb1f42382a22db7cddfa53190aa7ade4170
german120 <germanblasco120@gmail.com>
2023-05-11 16:30:34 -0300

    Corregido CORS

--- src/main/java/com/portfoliobackendap4/backendportfolio/Controller/PersonaController.java
+++ src/main/java/com/portfoliobackendap4/backendportfolio/Controller/PersonaController.java
@@ -18,7 +18,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
-@CrossOrigin(origins = {"**","http://localhost:4200"})
+@CrossOrigin(origins = {"https://portfolio-backend-ap4.onrender.com","http://localhost:4200"})
 public class PersonaController {
     @Autowired IPersonaService ipersonaService;
     
--- src/main/java/com/portfoliobackendap4/backendportfolio/Security/Controller/AuthController.java
+++ src/main/java/com/portfoliobackendap4/backendportfolio/Security/Controller/AuthController.java
@@ -34,7 +34,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/auth")
-@CrossOrigin(origins = {"**","http://localhost:4200"})
+@CrossOrigin(origins = {"https://portfolio-backend-ap4.onrender.com","http://localhost:4200"})
 
 public class AuthController {
     
